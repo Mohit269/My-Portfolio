e18f318e7fa1f91dc758aba65f925c98f95d5ee6
Mohit Sharma <mohitsharma26900@gmail.com>
2022-04-19 16:44:02 +0530

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import About from "./components/About";
 import Introduction from "./components/Introduction";
 function App() {
   return (
     <div>
       <Introduction />
+      <About />
     </div>
   );
 }
